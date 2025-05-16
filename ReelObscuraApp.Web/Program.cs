31f1feec6266fcacec366b273fa6eef7130be602
Nowaxial <Nowaxial@users.noreply.github.com>
2025-05-16 13:35:12 +0200

    Refactor Program class for improved structure
    Updated the `Program` class to enhance formatting and readability. The `Main` method's structure has been adjusted, removing the simple GET request mapping in favor of static file usage and controller mapping, aligning with a more robust web application architecture.

--- ReelObscuraApp.Web/Program.cs
+++ ReelObscuraApp.Web/Program.cs
@@ -1,15 +1,15 @@
 namespace ReelObscuraApp.Web
-{
-    public class Program
     {
-        public static void Main(string[] args)
+    public class Program
         {
+        public static void Main(string[] args)
+            {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
             var app = builder.Build();
-
-            app.MapGet("/", () => "Hello World!");
-
+            app.UseStaticFiles();
+            app.MapControllers();
             app.Run();
+            }
         }
     }
-}
