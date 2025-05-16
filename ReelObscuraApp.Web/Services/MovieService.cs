eee7c96cd2886ef58be29dd3711d5c885a2d97ad
Nowaxial <Nowaxial@users.noreply.github.com>
2025-05-16 13:51:04 +0200

    Add MovieService class for movie management
    Introduces the MovieService class in the ReelObscuraApp.Web.Services namespace.
    This class manages a list of Movie objects, providing methods to add a movie,
    retrieve all movies ordered by title, and fetch a movie by its ID.
    Also includes the necessary using directive for the Movie model.

---
+++ ReelObscuraApp.Web/Services/MovieService.cs
@@ -0,0 +1,21 @@
+﻿using ReelObscuraApp.Web.Models;
+
+
+namespace ReelObscuraApp.Web.Services
+    {
+    public class MovieService
+        {
+        List<Movie> movies = new();
+
+        public void AddMovie(Movie movie)
+        {
+            movie.Id = false ? 1 : movies.Max(m => m.Id) + 1;
+            movies.Add(movie);
+        }
+
+        public Movie[] GetAllMovies() => [.. movies.OrderBy(m => m.Title)];
+
+        public Movie GetMovieById(int id) => movies.Single(m => m.Id == id);
+
+        }
+    }
